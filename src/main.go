9d68b5f0e4a024460913afdb17107c3b446fe487
Reales Myles <rclarence123@gmail.com>
2023-04-21 17:41:51 -0500

    Operadores aritmerticos

--- src/main.go
+++ src/main.go
@@ -35,4 +35,80 @@ func main() {
 
 	fmt.Println("El área del cuadrado es: ", areaCuadrado)
 
+	x := 10
+	y := 50
+
+	//Suma
+
+	result := x + y
+
+	fmt.Println("El resultado de la suma es: ", result)
+
+	//Resta
+
+	result = y - x
+
+	fmt.Println("El resultado de la resta es: ", result)
+
+	//Multiplicación
+
+	result = x * y
+
+	fmt.Println("El resultado de la multiplicación es: ", result)
+
+	//División
+
+	result = y / x
+
+	fmt.Println("El resultado de la división es: ", result)
+
+	//Módulo
+
+	result = y % x
+
+	fmt.Println("El resultado del módulo es: ", result)
+
+	//Incremento
+
+	x++
+
+	fmt.Println("El valor de x es: ", x)
+
+	//Decremento
+
+	y--
+
+	fmt.Println("El valor de y es: ", y)
+
+	//Retos
+	// -Area de un triángulo, trapecio y círculo
+
+	//Area del triángulo
+	base1 := 10   //base del triángulo
+	altura1 := 15 //altura del triángulo
+
+	areaTriangulo := (base1 * altura1) / 2
+
+	fmt.Println("El área del triángulo es: ", areaTriangulo)
+
+	//Area del trapecio
+
+	baseMayor := 10
+	baseMenor := 5
+	alturaTrapecio := 15
+
+	resultadoTrapecio := ((baseMayor + baseMenor) * alturaTrapecio) / 2
+
+	fmt.Println("El área del trapecio es: ", resultadoTrapecio)
+
+	//Area del círculo
+
+	const pi3 = 3.1416
+
+	var radio float64 = 10
+
+	resultadoCirculo := pi3 * (radio * radio)
+
+	fmt.Println("El área del círculo es: ", resultadoCirculo)
+
 }
